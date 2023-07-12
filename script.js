ad0719a8aff8f76b9cff7df3313dfc88fa8f1d29
KANIMOZHI-J <129746211+KANIMOZHI-J@users.noreply.github.com>
2023-07-12 15:47:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function add(){
+    let a=parseInt(document.getElementById("n1").value);
+    let b=parseInt(document.getElementById("n2").value);
+    let c=a+b;
+    document.getElementById("button1").innerHTML="Addition is "+c;
+}
+function sub(){
+    let a=parseInt(document.getElementById("n1").value);
+    let b=parseInt(document.getElementById("n2").value);
+    let c=a-b;
+    document.getElementById("button2").innerHTML="Subtraction is "+c;
+}
+function mul(){
+    let a=parseInt(document.getElementById("n1").value);
+    let b=parseInt(document.getElementById("n2").value);
+    let c=a*b;
+    document.getElementById("button3").innerHTML="Muliplication is "+c;
+}
+function div(){
+    let a=parseInt(document.getElementById("n1").value);
+    let b=parseInt(document.getElementById("n2").value);
+    let c=a/b;
+    document.getElementById("button4").innerHTML="Division is "+c;
+}
+
